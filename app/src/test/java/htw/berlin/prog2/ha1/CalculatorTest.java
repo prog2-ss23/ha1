02d464a012e76ce9137b362453f82db8db6b807e
Ferdous Rahsepar <rahsepar19@gmail.com>
2023-04-12 18:49:47 +0200

    grüner Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -27,6 +27,23 @@ class CalculatorTest {
     }
 
     @Test
+    @DisplayName("should display result after substracting two numbers")
+    void testSubstration() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(1);
+        calc.pressDigitKey(0);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(5);
+        calc.pressEqualsKey();
+
+        String expected = "5";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
     @DisplayName("should display result after getting the square root of two")
     void testSquareRoot() {
         Calculator calc = new Calculator();
@@ -88,7 +105,5 @@ class CalculatorTest {
         assertEquals(expected, actual);
     }
 
-
-    //TODO hier weitere Tests erstellen
+    // TODO hier weitere Tests erstellen
 }
-
