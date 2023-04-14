70d7ada84d4e5c73e96b12c5498413726095036e
BuddeMax <Max.Budde@Student.HTW-Berlin.de>
2023-04-14 12:35:51 +0200

    second green test / code fixed after red test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -143,6 +143,27 @@ public class Calculator {
                 if (screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
                 if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
             }
+            if(latestOperation.equals("-")) {
+                Double result = Double.parseDouble(screen) - latestValue;
+                screen = Double.toString(result);
+                if (screen.equals("Infinity")) screen = "Error";
+                if (screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
+                if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+            }
+            if(latestOperation.equals("+")) {
+                Double result = latestValue + Double.parseDouble(screen);
+                screen = Double.toString(result);
+                if (screen.equals("Infinity")) screen = "Error";
+                if (screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
+                if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+            }
+            if(latestOperation.equals("x")) {
+                Double result = latestValue * Double.parseDouble(screen);
+                screen = Double.toString(result);
+                if (screen.equals("Infinity")) screen = "Error";
+                if (screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
+                if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+            }
         }
     }
 
