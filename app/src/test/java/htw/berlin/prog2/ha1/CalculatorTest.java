5b47d4eebaa74ace01c3c8955ddae71bfd6875fd
Ferdous Rahsepar <rahsepar19@gmail.com>
2023-04-20 10:59:03 +0200

    Final

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -27,23 +27,6 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("should display result after substracting two numbers")
-    void testSubstration() {
-        Calculator calc = new Calculator();
-
-        calc.pressDigitKey(1);
-        calc.pressDigitKey(0);
-        calc.pressBinaryOperationKey("-");
-        calc.pressDigitKey(5);
-        calc.pressEqualsKey();
-
-        String expected = "5";
-        String actual = calc.readScreen();
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
     @DisplayName("should display result after getting the square root of two")
     void testSquareRoot() {
         Calculator calc = new Calculator();
@@ -51,7 +34,7 @@ class CalculatorTest {
         calc.pressDigitKey(2);
         calc.pressUnaryOperationKey("√");
 
-        String expected = "1.41421356";
+        String expected = "1.4142136";
         String actual = calc.readScreen();
 
         assertEquals(expected, actual);
@@ -106,6 +89,23 @@ class CalculatorTest {
     }
 
     // TODO hier weitere Tests erstellen
+
+    @Test
+    @DisplayName("should display result after substracting two numbers")
+    void testSubstration() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(1);
+        calc.pressDigitKey(0);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(5);
+        calc.pressEqualsKey();
+
+        String expected = "5";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 
     @Test
     @DisplayName("should display result 9 (not 9.0) after getting the square root of 81")
