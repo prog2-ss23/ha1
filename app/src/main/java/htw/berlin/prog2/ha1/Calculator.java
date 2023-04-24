9de699647bfb8cba22f1af60861a07d0dc1d5e66
Hagen Hausser <Hagen.Hausser@Student.HTW-Berlin.de>
2023-04-24 23:53:39 +0200

    Red Test 2 bug fix

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -59,6 +59,9 @@ public class Calculator {
      * @param operation "+" für Addition, "-" für Substraktion, "x" für Multiplikation, "/" für Division
      */
     public void pressBinaryOperationKey(String operation)  {
+        if (latestValue != 0.0) {
+            pressEqualsKey();
+        }
         latestValue = Double.parseDouble(screen);
         latestOperation = operation;
     }
