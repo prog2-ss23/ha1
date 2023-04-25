64b17f0293785030b4e08ceaf770fc826ddcb19c
Noam Shani <Noams156@gmail.com>
2023-04-25 11:17:19 +0200

    last bug fixed , no more decimals for clean squares

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -88,6 +88,8 @@ public class Calculator {
         screen = Double.toString(result);
         if(screen.equals("NaN")) screen = "Error";
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
+
 
     }
 
