65e5b4101808600fb0c0b7d740d3c29beb1fc2d3
Noam Shani <Noams156@gmail.com>
2023-04-25 11:12:27 +0200

    bug nr 2 found - square root contains decimal

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -129,5 +129,21 @@ class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    @DisplayName("Should display an integer for a square root") 
+     void testRoundedSquare() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(6);
+        calc.pressDigitKey(4);
+        calc.pressUnaryOperationKey("√");
+
+
+        String expected = "8";
+        String actual = calc.readScreen();
+        assertEquals(expected, actual);
+
+
+}
 }
 
