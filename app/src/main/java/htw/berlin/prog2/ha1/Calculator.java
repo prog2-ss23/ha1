34e3a3f7fcbadc817d366e9ad4267d6bf8be2418
Sawin Saeed <Sawin.Saeed@Student.Htw.Berlin.de>
2023-04-26 00:05:15 +0200

    Aufgabe 2 roter Test Nr.2 gelöst

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -130,5 +130,6 @@ public class Calculator {
         if(screen.equals("Infinity")) screen = "Error";
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        if(screen.equals("-0")) screen = "0";
     }
 }
