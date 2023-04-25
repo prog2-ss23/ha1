b7a11ddb67439a17ff71cdb62387587eb5b6325b
LamaChihabi <107999296+LamaChihabi@users.noreply.github.com>
2023-04-25 23:34:27 +0200

    Commit für Fix zum Test testEqualButton.

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -121,17 +121,18 @@ public class Calculator {
      */
     public void pressEqualsKey() {
 
-
-
+        double currentValue = Double.parseDouble(screen);
         var result = switch(latestOperation) {
 
-            case "+" -> latestValue + Double.parseDouble(screen) ;
-            case "-" -> latestValue - Double.parseDouble(screen);
-            case "x" -> latestValue * Double.parseDouble(screen);
-            case "/" -> latestValue / Double.parseDouble(screen);
+            case "+" -> latestValue + currentValue ;
+            case "-" -> latestValue - currentValue;
+            case "x" -> latestValue * currentValue;
+            case "/" -> latestValue / currentValue;
 
             default -> throw new IllegalArgumentException();
         };
+
+        latestValue = currentValue;
         screen = Double.toString(result);
         if(screen.equals("Infinity")) screen = "Error";
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
