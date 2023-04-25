09d54f4e31a102154211fa94537b3d51134de4ca
Paul Böttcher~ <paul.boettcher03@outlook.de~>
2023-04-25 15:01:40 +0200

    aufgabe 1, funktionierender test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -90,5 +90,20 @@ class CalculatorTest {
 
 
     //TODO hier weitere Tests erstellen
+
+    @Test
+    @DisplayName("should display result when multiplicating two positive numbers")
+    void testPositiveMultiplication() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(5);
+        calc.pressBinaryOperationKey("x");
+        calc.pressDigitKey(4);
+        calc.pressEqualsKey();
+
+        String expected = "20";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 }
 
