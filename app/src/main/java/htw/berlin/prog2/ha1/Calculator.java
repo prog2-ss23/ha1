9fd4b1f20d07e69e5dd44cbf7bd7973e15dbfa9c
s0569241KG <115718025+s0569241KG@users.noreply.github.com>
2023-04-25 23:47:40 +0200

    5. bugfix of 4th commit

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -82,6 +82,7 @@ public class Calculator {
         };
         screen = Double.toString(result);
         if(screen.equals("NaN")) screen = "Error";
+        if(screen.equals("Infinity")) screen = "Error";
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
 
     }
