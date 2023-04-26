2054b3817a89b32c6b28606bbf040491ba508937
Max Gurgel <51488459+s0569230@users.noreply.github.com>
2023-04-26 18:03:06 +0200

    2nd Fix
    Fixing 2nd red test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -134,6 +134,7 @@ public class Calculator {
             case "-" -> latestValue - Double.parseDouble(screen);
             case "x" -> latestValue * Double.parseDouble(screen);
             case "/" -> latestValue / Double.parseDouble(screen);
+            case "" -> latestValue = Double.parseDouble(screen);
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
