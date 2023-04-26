9f36bf69265727bb2a20bd5062601ed173d9670d
Zeroo01 <zeren.kanber@web.de>
2023-04-26 06:47:42 +0200

    Teilaufgabe 1: neu grüner Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -88,6 +88,25 @@ class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    //1. Neuer grüner Test
+    @Test
+    @DisplayName("should display result after multiplying two positive numbers")
+    void testPositiveMultiplication() {
+        Calculator calc = new Calculator();
+    
+        calc.pressDigitKey(7);
+        calc.pressBinaryOperationKey("x");
+        calc.pressDigitKey(1);
+        calc.pressDigitKey(0);
+        calc.pressEqualsKey();
+    
+        String expected = "70";
+        String actual = calc.readScreen();
+    
+        assertEquals(expected, actual);
+    }
+
+
 
     //TODO hier weitere Tests erstellen
 }
