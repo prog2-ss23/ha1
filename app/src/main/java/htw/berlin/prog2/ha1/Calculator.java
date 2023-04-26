50388f54ce1afd3c81eced3371b577204159d963
Zeroo01 <zeren.kanber@web.de>
2023-04-26 06:53:24 +0200

    Teilaufgabe 3: Fix des ersten roten Tests

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -84,6 +84,7 @@ public class Calculator {
         if(screen.equals("NaN")) screen = "Error";
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
 
+        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length() - 2);
     }
 
     /**
