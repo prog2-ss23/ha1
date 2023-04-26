fe79eed726965552634872723df7fa22ed13e0ff
kemalghifari <mkemalgr@gmail.com>
2023-04-26 22:03:41 +0200

    third green test: fix to the second red test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -31,7 +31,7 @@ public class Calculator {
     public void pressDigitKey(int digit) {
         if(digit > 9 || digit < 0) throw new IllegalArgumentException();
 
-        if(screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
+        if(screen.equals("0") || latestValue == Double.parseDouble(screen) && !screen.endsWith(".")) screen = "";
 
         screen = screen + digit;
     }
