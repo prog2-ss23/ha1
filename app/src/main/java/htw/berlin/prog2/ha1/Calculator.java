650cc815f1738b98e843fb2d29d39936d61fa1b6
Felicitas Faupel <fefaupel@outlook.de>
2023-10-22 19:50:46 +0200

    Roter test 2 Fix

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -60,6 +60,7 @@ public class Calculator {
      * @param operation "+" für Addition, "-" für Substraktion, "x" für Multiplikation, "/" für Division
      */
     public void pressBinaryOperationKey(String operation)  {
+        if(latestOperation != "") pressEqualsKey(); //aktualiesiert Zwischensumme auf screen und überprüft aufeinanderfolgende Rechenzeichen
         latestValue = Double.parseDouble(screen);
         latestOperation = operation;
     }
